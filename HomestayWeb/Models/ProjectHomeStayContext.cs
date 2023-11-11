8c9f94b7b40d79b4fb7b04eccc26a5b8cd868d14
ThanhDq8902 <quangthanh08092002@gmail.com>
2023-11-12 04:27:11 +0700

    sua giao dien

--- HomestayWeb/Models/ProjectHomeStayContext.cs
+++ HomestayWeb/Models/ProjectHomeStayContext.cs
@@ -31,7 +31,7 @@ public partial class ProjectHomeStayContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=LAPTOP-LQOFJPF7\\SQLEXPRESS;Initial Catalog=Project_HomeStay; User ID=sa;Password=12345;Encrypt=false;TrustServerCertificate=true");
+        => optionsBuilder.UseSqlServer("server=(local);database=Project_HomeStay;uid=sa;pwd=123;TrustServerCertificate=true");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
