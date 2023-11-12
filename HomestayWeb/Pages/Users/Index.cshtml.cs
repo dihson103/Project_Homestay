63db0a813eae9e6d6a39eda4627eb2083bd90ccf
Nguyễn Đình Sơn <120614021+dihson103@users.noreply.github.com>
2023-11-12 21:09:41 +0700

    add search users

--- HomestayWeb/Pages/Users/Index.cshtml.cs
+++ HomestayWeb/Pages/Users/Index.cshtml.cs
@@ -11,6 +11,13 @@ using HomestayWeb.Utils;
 
 namespace HomestayWeb.Pages.Users
 {
+    public class UserSearchDto
+    {
+        public string SearchValue { get; set; }
+        public string Role { get; set; }
+        public bool Status { get; set; }
+    }
+
     [Authorize(Policy = "Admin")]
     public class IndexModel : PageModel
     {
@@ -22,23 +29,57 @@ namespace HomestayWeb.Pages.Users
         }
 
         public PaginatedList<User> User { get; set; } = default!;
+        [BindProperty]
+        public UserSearchDto SearchDto { get; set; }
+        private int pageSize = 1;
+        
 
         public async Task OnGetAsync(int? pageIndex)
         {
-            int pageSize = 1;
             if (pageIndex == null)
             {
                 pageSize = 1;
             }
 
+            SearchDto = new UserSearchDto()
+            {
+                Role = "USER",
+                Status = true
+            };
+
             if (_context.Users != null)
             {
-                var users = _context.Users;
+                var users = _context.Users.Where(u => u.Status == SearchDto.Status && u.Role == SearchDto.Role);
                 User = await PaginatedList<User>.CreateAsync(
                             users.AsNoTracking(),
                             pageIndex ?? 1, pageSize
                          );
             }
+        }
+
+        public async Task OnPostAsync(int? pageIndex)
+        {
+            if (pageIndex == null)
+            {
+                pageSize = 1;
+            }
+
+            var query = _context.Users.Where(u => u.Status == SearchDto.Status && u.Role == SearchDto.Role);
+
+            if(!string.IsNullOrEmpty(SearchDto.SearchValue))
+            {
+                var searchValue = SearchDto.SearchValue.ToLower();
+                query = query.Where(x => x.Fullname.ToLower().Contains(searchValue) 
+                            || x.Username.ToLower().Contains(searchValue)
+                            || x.Email.ToLower().Contains(searchValue)
+                        );
+            }
+
+            User = await PaginatedList<User>.CreateAsync(
+                             query.AsNoTracking(),
+                             pageIndex ?? 1, pageSize
+                          );
+
         }
     }
 }
