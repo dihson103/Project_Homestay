335df76ca0c5604e2a8cdab200d613809a02971e
Nguyễn Đình Sơn <120614021+dihson103@users.noreply.github.com>
2023-11-12 20:18:23 +0700

    search homestay in home page and homestay index

--- HomestayWeb/Pages/HomeStays/Index.cshtml.cs
+++ HomestayWeb/Pages/HomeStays/Index.cshtml.cs
@@ -11,6 +11,15 @@ using HomestayWeb.Utils;
 
 namespace HomestayWeb.Pages.HomeStays
 {
+    public class Search
+    {
+        public string SearchString { get; set; }
+        public string City { get; set; }
+        public string District { get; set; }
+        public string Ward { get; set; }
+        public bool Status { get; set; }
+    }
+
     [Authorize(Policy = "Admin")]
     public class IndexModel : PageModel
     {
@@ -22,10 +31,12 @@ namespace HomestayWeb.Pages.HomeStays
         }
 
         public PaginatedList<Homestay> Homestay { get; set; } = default!;
+        [BindProperty]
+        public Search Search { get; set; } = default!;
+        private int pageSize = 1;
 
         public async Task OnGetAsync(int? pageIndex)
         {
-            int pageSize = 1;
             if(pageIndex == null)
             {
                 pageIndex = 1;
@@ -40,6 +51,40 @@ namespace HomestayWeb.Pages.HomeStays
                             pageIndex ?? 1, pageSize
                          );
             }
+            Search = new Search()
+            {
+                Status = true
+            };
+        }
+
+        public async Task OnPostAsync(int? pageIndex)
+        {
+            var query = _context.Homestays.Include(h => h.Images).Where(h => h.Status == Search.Status);
+
+            if (!string.IsNullOrEmpty(Search?.SearchString))
+            {
+                query = query.Where(h => h.HomestayName.Contains(Search.SearchString));
+            }
+
+            if (!string.IsNullOrEmpty(Search?.City))
+            {
+                query = query.Where(h => h.City.Equals(Search.City));
+            }
+
+            if (!string.IsNullOrEmpty(Search?.District))
+            {
+                query = query.Where(h => h.District.Equals(Search.District));
+            }
+
+            if (!string.IsNullOrEmpty(Search?.Ward))
+            {
+                query = query.Where(h => h.Commune.Equals(Search.Ward));
+            }
+
+            Homestay = await PaginatedList<Homestay>.CreateAsync(
+                            query.AsNoTracking(),
+                            pageIndex ?? 1, pageSize
+                         );
         }
     }
 }
--- HomestayWeb/Pages/Index.cshtml.cs
+++ HomestayWeb/Pages/Index.cshtml.cs
@@ -2,9 +2,18 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 namespace HomestayWeb.Pages
 {
+    public class SearchDto
+    {
+        public string SearchString { get; set; }
+        public string City { get; set; }
+        public string District { get; set; }
+        public string Ward { get; set; }
+    } 
+
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
@@ -17,6 +26,8 @@ namespace HomestayWeb.Pages
         }
 
         public List<Homestay> Homestays { get; set; }
+        [BindProperty]
+        public SearchDto Search { get; set; } = default!;
 
         public void OnGet()
         {
@@ -24,6 +35,36 @@ namespace HomestayWeb.Pages
                 .Include(h => h.Images)
                 .Where(h => h.Status)
                 .ToList();
+            Search = new SearchDto();
         }
+
+        public void OnPost()
+        {
+            var query = _context.Homestays.Include(h => h.Images).Where(h => h.Status);
+
+            if (!string.IsNullOrEmpty(Search?.SearchString))
+            {
+                query = query.Where(h => h.HomestayName.Contains(Search.SearchString));
+            }
+
+            if (!string.IsNullOrEmpty(Search?.City))
+            {
+                query = query.Where(h => h.City.Equals(Search.City));
+            }
+
+            if (!string.IsNullOrEmpty(Search?.District))
+            {
+                query = query.Where(h => h.District.Equals(Search.District));
+            }
+
+            if (!string.IsNullOrEmpty(Search?.Ward))
+            {
+                query = query.Where(h => h.Commune.Equals(Search.Ward));
+            }
+
+            Homestays = query.ToList();
+        }
+
+
     }
 }
